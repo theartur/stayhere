d77d195de10236caf7239a3bbceaf42745292ced
n33x <artur.alves@gmail.com>
2015-05-13 06:15:28 +0000

    StayHERE is born ! ;}-

--- avatar.js
+++ avatar.js
@@ -3,7 +3,10 @@
 WorldWideFreedom.avatar = {
     init: function () {
         var watermark = document.createElement("div");
+        var here = document.createElement("span");
+        here.innerHTML = "here";
         watermark.id = "watermark";
+        watermark.appendChild(here);
         WorldWideFreedom.utils.insert(document.body, watermark);
     }
 };
